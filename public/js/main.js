047aa7dfd95d6937c082862e3635542a5f4e5b43
Luan <luangrossert@gmail.com>
2024-06-09 20:48:05 -0300

    cria projeto / cria passo

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,6 @@
 async function criaProjeto() {
     try {
-        const novoProjeto = await fetch('/app/br.com.frontierless.view/projetoUsuario.html');
+        const novoProjeto = await fetch('/app/br.com.frontierless.view/perfil/projetoUsuario.html');
         const data = await novoProjeto.text();
 
         const startSectionIndex = data.indexOf('<section>');
@@ -11,23 +11,57 @@ async function criaProjeto() {
         const rightProjetosUsuario = document.getElementById('rightProjetosUsuario');
 
         limpaContainer(rightProjetosUsuario),
-        leftProjetosUsuario.innerHTML += primeiraSection;
+        leftProjetosUsuario.innerHTML += primeiraSection;  
         rightProjetosUsuario.innerHTML += data;
+        setDataInicioProjeto()
+        disabledLeftSecction();
     } catch (error) {
         console.error(error.message);
     }
-}
+};
 
-function limpaContainer(rightProjetosUsuario){
-    const sectionToRemove = rightProjetosUsuario.querySelectorAll('section');
-    sectionToRemove.forEach(section => rightProjetosUsuario.removeChild(section));   
-}
+async function criaPassoProjeto() {
+    try {
+        const passoProjeto = await fetch('/app/br.com.frontierless.view/perfil/passoProjeto.html');
+        const data = await passoProjeto.text();
+        const passoLista = document.getElementById('passos');
+        passoLista.innerHTML += data;
+    } catch (error) {
+        console.error(error.message);
+    }
+};
+
+function limpaContainer(container){
+    container.innerHTML = '';
+};
 
 function expandeContainer(oEvent){
     const containerPasso = oEvent.currentTarget.parentElement;
-    const arw = containerPasso.querySelector("header div");
+    const hdr = containerPasso.querySelector("ol li header");
+    const arw = hdr.querySelector(".rotA");
+
     containerPasso.classList.toggle("expandContainer");
     arw.classList.toggle("rotateArrow");
+};
 
-    console.log(oEvent)
-}
+function setDataInicioProjeto(){
+    const dataInicio = document.getElementById('dataInicio');
+    const currentDate = new Date();
+    const formattedDate = currentDate.toLocaleDateString('pt-BR', {
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric'
+    });
+    dataInicio.textContent = formattedDate;
+};
+
+function disabledLeftSecction(){
+    const leftSection = document.getElementById('leftProjetosUsuario');
+    const section = leftSection.querySelectorAll('section');
+    for(let i = 0;i<section.length;i++){
+        const inputsAndSelects = section[i].querySelectorAll('input, select');
+        inputsAndSelects.forEach(element => {
+            element.disabled = true;
+        });
+    }
+};
