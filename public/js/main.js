25eed4919c4af0a401bba99774692accb80a9dcd
Luan <luangrossert@gmail.com>
2024-06-26 17:38:41 -0300

    implementacao bootstrap

--- public/js/main.js
+++ public/js/main.js
@@ -1,67 +1,111 @@
-async function criaProjeto() {
-    try {
-        const novoProjeto = await fetch('/app/br.com.frontierless.view/perfil/projetoUsuario.html');
-        const data = await novoProjeto.text();
+function criaProjeto() {
+    let pjs = document.getElementById('listaProjetos');
+    const codigoHtml =
+        `
+        <div class="card mt-3" onclick="editarProjeto()">
+            <div class="card-header bg-dark text-light">
+                <h3>Australia</h3>
+            </div>
+            <div class="card-body container bg-light">
+                <div class="row">
+                    <div class="col-md-6">
+                        <div class="mb-2 text-center">
+                            <label for="statusInput" class="form-label">Status</label>
+                            <input type="text" id="statusInput" class="form-control" Disabled>
+                        </div>
+                        <div class="mb-2 text-center">
+                            <label for="tipoInput" class="form-label">Tipo</label>
+                            <input type="text" id="tipoInput" class="form-control" Disabled>
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="mb-2 text-center">
+                            <label for="valorNecessarioInput" class="form-label">Valor Necessário</label>
+                            <input type="text" id="valorNecessarioInput" class="form-control" Disabled>
+                        </div>
+                        <div class="mb-2 text-center">
+                            <label for="valorGuardadoInput" class="form-label">Valor Guardado</label>
+                            <input type="text" id="valorGuardadoInput" class="form-control " Disabled>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
 
-        const startSectionIndex = data.indexOf('<section>');
-        const endSectionIndex = data.indexOf('</section>', startSectionIndex);
-        const primeiraSection = data.slice(startSectionIndex, endSectionIndex + '</section>'.length);
+        `;
+    pjs.innerHTML += codigoHtml;
 
-        const leftProjetosUsuario = document.getElementById('leftProjetosUsuario');
-        const rightProjetosUsuario = document.getElementById('rightProjetosUsuario');
+}
 
-        limpaContainer(rightProjetosUsuario),
-        leftProjetosUsuario.innerHTML += primeiraSection;  
-        rightProjetosUsuario.innerHTML += data;
-        setDataInicioProjeto()
-        disabledLeftSecction();
-    } catch (error) {
-        console.error(error.message);
-    }
-};
+function editarProjeto() {
+    criaPasso()
+    criaPasso()
+    criaPasso()
+    criaPasso()
+    criaPasso()
+}
 
-async function criaPassoProjeto() {
-    try {
-        const passoProjeto = await fetch('/app/br.com.frontierless.view/perfil/passoProjeto.html');
-        const data = await passoProjeto.text();
-        const passoLista = document.getElementById('passos');
-        passoLista.innerHTML += data;
-    } catch (error) {
-        console.error(error.message);
-    }
-};
+var cont = 0;
+function criaPasso() {
+    let pjs = document.getElementById('editorProjeto');
+    const codigoHtml =
+        `
+        <div class="accordion accordion-flush" id="accordionFlushExample${cont}">
+            <div class="accordion-item mt-3 border rounded">
+                <button class="accordion-header accordion-button collapsed" type="button" data-bs-toggle="collapse"
+                data-bs-target="#flush-collapseOne${cont}" aria-expanded="false" aria-controls="flush-collapseOne${cont}">
+                    <div class="input-group">
+                        <div class="input-group-text rounded me-3">
+                            <input class="form-check-input mt-0 " type="checkbox" value=""
+                                aria-label="Checkbox for following text input">
+                        </div>
+                        <input type="text" class="border border-top-0 border-end-0 border-start-0 rounded-end" aria-label="Text input with checkbox">
+                    </div>
 
-function limpaContainer(container){
-    container.innerHTML = '';
-};
+                </button>
+                <div id="flush-collapseOne${cont}" class="accordion-collapse collapse"
+                    data-bs-parent="#accordionFlushExample${cont}">
+                    <div class="accordion-body">
+                        <div class="input-group mb-3">
+                            <label class="me-3 d-flex align-items-center">Custo</label>
+                            <span class="input-group-text rounded-start">R$</span>
+                            <input type="text" class="form-control" aria-label="Amount (to the nearest dollar)">
+                        </div>
+                        <div class="input-group mb-3">
+                            <label class="me-3 d-flex align-items-center">Local</label>
+                            <input type="text" class="form-control rounded"
+                                aria-label="Amount (to the nearest dollar)">
+                        </div>
+                        <label class="me-3 d-flex align-items-center">Descrição</label>
+                        <div class="input-group">
+                            <textarea class="form-control" aria-label="With textarea"></textarea>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        `
+    pjs.innerHTML += codigoHtml;
+    cont++;
+}
 
-function expandeContainer(oEvent){
-    const containerPasso = oEvent.currentTarget.parentElement;
-    const hdr = containerPasso.querySelector("ol li header");
-    const arw = hdr.querySelector(".rotA");
+function statusInpt(oEvent) {
+    let listaProjetos = document.getElementById('listaProjetos');
+    let inputs = listaProjetos.querySelectorAll('input');
 
-    containerPasso.classList.toggle("expandContainer");
-    arw.classList.toggle("rotateArrow");
-};
-
-function setDataInicioProjeto(){
-    const dataInicio = document.getElementById('dataInicio');
-    const currentDate = new Date();
-    const formattedDate = currentDate.toLocaleDateString('pt-BR', {
-        year: 'numeric',
-        month: 'long',
-        day: 'numeric'
+    inputs.forEach(input => {
+        if (!oEvent.target.checked) {
+            input.removeAttribute('disabled');
+        } else {
+            input.setAttribute('disabled', 'true');
+        }
     });
-    dataInicio.textContent = formattedDate;
-};
 
-function disabledLeftSecction(){
-    const leftSection = document.getElementById('leftProjetosUsuario');
-    const section = leftSection.querySelectorAll('section');
-    for(let i = 0;i<section.length;i++){
-        const inputsAndSelects = section[i].querySelectorAll('input, select');
-        inputsAndSelects.forEach(element => {
-            element.disabled = true;
-        });
-    }
-};
+
+}
+
+
+
+
+
+
